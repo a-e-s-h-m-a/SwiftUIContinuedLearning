4f8b10da55b956bc786ce48e70ca46608335c6a6
Aeshma <madhushanka2727@gmail.com>
2024-01-16 21:27:24 +0530

    longpress example

--- ContinuedLearning/LongPressGuestureBootcamp.swift
+++ ContinuedLearning/LongPressGuestureBootcamp.swift
@@ -10,19 +10,70 @@ import SwiftUI
 struct LongPressGuestureBootcamp: View {
     
     @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
     
     var body: some View {
-        Text(isComplete ? "COMPLETED" : "NOT COMPLETE")
-            .padding()
-            .padding(.horizontal)
-            .background(isComplete ? Color.green : Color.gray)
-            .cornerRadius(10)
-//            .onTapGesture {
+        
+        VStack {
+            Rectangle()
+                .fill(isSuccess ? .green : .blue)
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(.gray)
+            
+            HStack {
+                Text("CLICK HERE")
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) {
+                        // at the min duration
+                        withAnimation(.easeInOut) {
+                            isSuccess = true
+                        }
+                    } onPressingChanged: { isPressing in
+                        // start of press -> min duration
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                isComplete = true
+                            }
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess {
+                                    withAnimation(.easeInOut) {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                
+                Text("RESET")
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        isComplete = false
+                        isSuccess = false
+                    }
+            }
+        }
+        
+//        Text(isComplete ? "COMPLETED" : "NOT COMPLETE")
+//            .padding()
+//            .padding(.horizontal)
+//            .background(isComplete ? Color.green : Color.gray)
+//            .cornerRadius(10)
+////            .onTapGesture {
+////                isComplete.toggle()
+////            }
+//            .onLongPressGesture(minimumDuration: 2.0,  maximumDistance: 50) {
 //                isComplete.toggle()
 //            }
-            .onLongPressGesture(minimumDuration: 2.0,  maximumDistance: 50) {
-                isComplete.toggle()
-            }
     }
 }
 
