41af582fd2792b6cddec4101bacaf9fd3bfea0e1
Aeshma <madhushanka2727@gmail.com>
2024-01-16 22:18:42 +0530

    magnification gesture example

--- MagnificationGestureBootcamp.swift
+++ MagnificationGestureBootcamp.swift
@@ -13,21 +13,55 @@ struct MagnificationGestureBootcamp: View {
     @State var lastAmount: CGFloat = 0
     
     var body: some View {
-        Text("Hello, World!")
-            .font(.title)
-            .padding()
-            .background(Color.red.cornerRadius(10))
-            .scaleEffect(1 + currentAmount + lastAmount)
-            .gesture(
-                MagnificationGesture()
-                    .onChanged{ value in
-                        currentAmount = value - 1
-                    }
-                    .onEnded { value in
-                        lastAmount += currentAmount
-                        currentAmount = 0
-                    }
-            )
+        VStack(spacing: 10) {
+            HStack {
+                Circle().frame(width: 35, height: 35)
+                Text("Swiftful Thinking")
+                Spacer()
+                Image(systemName: "ellipsis")
+            }
+            .padding(.horizontal)
+            Rectangle()
+                .frame(height: 300)
+                .scaleEffect(1 + currentAmount)
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged { value in
+                            currentAmount = value - 1
+                        }
+                        .onEnded { value in
+                            withAnimation(.spring()) {
+                                currentAmount = 0
+                            }
+                        }
+                )
+            HStack {
+                Image(systemName: "heart.fill")
+                Image(systemName: "text.bubble.fill")
+                Spacer()
+            }
+            .padding(.horizontal)
+            .font(.headline)
+            Text("This is the caption of my photo...")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+        }
+        
+//        Text("Hello, World!")
+//            .font(.title)
+//            .padding()
+//            .background(Color.red.cornerRadius(10))
+//            .scaleEffect(1 + currentAmount + lastAmount)
+//            .gesture(
+//                MagnificationGesture()
+//                    .onChanged{ value in
+//                        currentAmount = value - 1
+//                    }
+//                    .onEnded { value in
+//                        lastAmount += currentAmount
+//                        currentAmount = 0
+//                    }
+//            )
     }
 }
 
