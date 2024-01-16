3c18743b7fd658f15b5ed68cfbee2be06e71c9de
Aeshma <madhushanka2727@gmail.com>
2024-01-16 20:43:29 +0530

    first commit

---
+++ ContinuedLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContinuedLearning
+//
+//  Created by Malith Madhushanka on 2024-01-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ContinuedLearning/ContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedLearningApp.swift
+//  ContinuedLearning
+//
+//  Created by Malith Madhushanka on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
