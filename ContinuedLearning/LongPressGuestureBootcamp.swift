5723360a1a21e8a0a337a84f76e648bb7b9e5a6f
Aeshma <madhushanka2727@gmail.com>
2024-01-16 20:53:32 +0530

    normal tap gesture

---
+++ ContinuedLearning/LongPressGuestureBootcamp.swift
@@ -0,0 +1,28 @@
+//
+//  LongPressGuestureBootcamp.swift
+//  ContinuedLearning
+//
+//  Created by Malith Madhushanka on 2024-01-16.
+//
+
+import SwiftUI
+
+struct LongPressGuestureBootcamp: View {
+    
+    @State var isComplete: Bool = false
+    
+    var body: some View {
+        Text(isComplete ? "COMPLETED" : "NOT COMPLETE")
+            .padding()
+            .padding(.horizontal)
+            .background(isComplete ? Color.green : Color.gray)
+            .cornerRadius(10)
+            .onTapGesture {
+                isComplete.toggle()
+            }
+    }
+}
+
+#Preview {
+    LongPressGuestureBootcamp()
+}
