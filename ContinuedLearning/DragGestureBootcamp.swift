96f9b6514415110f562485ad3f4abf093a118884
Aeshma <madhushanka2727@gmail.com>
2024-01-17 19:45:43 +0530

    drag gesture example 1

--- ContinuedLearning/DragGestureBootcamp.swift
+++ ContinuedLearning/DragGestureBootcamp.swift
@@ -12,22 +12,47 @@ struct DragGestureBootcamp: View {
     @State var offset: CGSize = .zero
     
     var body: some View {
-        RoundedRectangle(cornerRadius: 20)
-            .frame(width: 100, height: 100)
-            .offset(offset)
-            .gesture(
-                DragGesture()
-                    .onChanged { value in
-                        withAnimation(.spring) {
-                            offset = value.translation
+        ZStack {
+            VStack {
+                Text("\(offset.width)")
+                    Spacer()
+            }
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 300, height: 500)
+                .offset(offset)
+                .scaleEffect(getScaledAmount())
+                .rotationEffect(Angle(degrees: getRotationAmount()))
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.spring) {
+                                offset = value.translation
+                            }
                         }
-                    }
-                    .onEnded { value in
-                        withAnimation(.spring) {
-                            offset = .zero
+                        .onEnded { value in
+                            withAnimation(.spring) {
+                                offset = .zero
+                            }
                         }
-                    }
             )
+        }
+    }
+    
+    func getScaledAmount() -> CGFloat {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount / max
+        return 1.0 - min(percentage, 0.5) * 0.5
+    }
+    
+    func getRotationAmount() -> Double {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = offset.width
+        let percentage = currentAmount / max
+        let percentageAsDouble = Double(percentage)
+        let maxAngle = 10.0
+        return percentageAsDouble * maxAngle
     }
 }
 
