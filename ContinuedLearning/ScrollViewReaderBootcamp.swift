bccb16cc800e3e724a1c50077a7cc1d9e7d1a17c
Aeshma <madhushanka2727@gmail.com>
2024-01-17 21:31:59 +0530

    scrollview reader scroll to a place

--- ContinuedLearning/ScrollViewReaderBootcamp.swift
+++ ContinuedLearning/ScrollViewReaderBootcamp.swift
@@ -8,25 +8,44 @@
 import SwiftUI
 
 struct ScrollViewReaderBootcamp: View {
+    
+    @State var scrollToIndex: Int = 0
+    @State var textFieldText = ""
+    
     var body: some View {
-        ScrollView {
-            ScrollViewReader { proxy in
-                Button("CLICK HERE TO GO TO #30") {
-                    withAnimation(.spring()) {
-                        proxy.scrollTo(30, anchor: .bottom)
+        VStack {
+            TextField("Enter a # here...", text: $textFieldText)
+                .frame(height: 55)
+                .border(Color.gray)
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            Button("SCROLL NOW") {
+                withAnimation(.spring()) {
+                    if let index = Int(textFieldText) {
+                        scrollToIndex = index
                     }
                 }
-                
-                ForEach(0..<50) { index in
-                    Text("This is item #\(index)")
-                        .font(.headline)
-                        .frame(height: 200)
-                        .frame(maxWidth: .infinity)
-                        .background(.white)
-                        .cornerRadius(10)
-                        .shadow(radius: 10)
-                        .padding()
-                        .id(index)
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    ForEach(0..<50) { index in
+                        Text("This is item #\(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex) { value in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(value, anchor: .top)
+                        }
+                    }
                 }
             }
         }
