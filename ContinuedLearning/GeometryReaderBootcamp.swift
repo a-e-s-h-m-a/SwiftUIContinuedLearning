d7a1f4b38921e713d9e2bc3add00ff8b3d53f65a
Aeshma <madhushanka2727@gmail.com>
2024-01-22 22:49:07 +0530

    with geometry reader

--- ContinuedLearning/GeometryReaderBootcamp.swift
+++ ContinuedLearning/GeometryReaderBootcamp.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct GeometryReaderBootcamp: View {
     var body: some View {
-        HStack(spacing: 0) {
-            Rectangle().fill(.red)
-                .frame(width: UIScreen.main.bounds.width * 0.6666)
-            Rectangle().fill(.blue)
+        GeometryReader { geometry in
+            HStack(spacing: 0) {
+                Rectangle().fill(.red)
+                    .frame(width: geometry.size.width * 0.6666)
+                    //.frame(width: UIScreen.main.bounds.width * 0.6666) // not adapting to the device rotation
+                Rectangle().fill(.blue)
+            }
+            .ignoresSafeArea()
         }
-        .ignoresSafeArea()
     }
 }
 
