670ddcc7df68e784ee4f131cb3d511f0ffafe73c
Aeshma <madhushanka2727@gmail.com>
2024-01-23 22:47:07 +0530

    basic way

---
+++ ContinuedLearning/MaskBootcamp.swift
@@ -0,0 +1,29 @@
+//
+//  MaskBootcamp.swift
+//  ContinuedLearning
+//
+//  Created by Malith Madhushanka on 2024-01-23.
+//
+
+import SwiftUI
+
+struct MaskBootcamp: View {
+    
+    @State var rating: Int = 3
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                ForEach(1..<6){ index in
+                    Image(systemName: "star.fill")
+                        .font(.largeTitle)
+                        .foregroundStyle(rating >= index ? .yellow : .gray)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MaskBootcamp()
+}
