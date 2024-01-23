9c37d2587814103deee1813c11f5698fbd4ca43b
Aeshma <madhushanka2727@gmail.com>
2024-01-23 22:32:26 +0530

    solving the problem method 1 - using multiple sheets

--- ContinuedLearning/MultipleSheetBootcamp.swift
+++ ContinuedLearning/MultipleSheetBootcamp.swift
@@ -21,38 +21,45 @@ struct MultipleSheetBootcamp: View {
     
     @State var selectedModel: RandomModel = RandomModel(title: "STARTING TITLE")
     @State var showSheet: Bool = false
+    @State var showSheet2: Bool = false
 //    @State var selectedIndex: Int = 0
     
     var body: some View {
         VStack(spacing: 20) {
             Button("Button 1") {
 //                selectedIndex = 1
-                selectedModel = RandomModel(title: "ONE")
+//                selectedModel = RandomModel(title: "ONE")
                 showSheet.toggle()
             }
+            .sheet(isPresented: $showSheet, content: {
+                NextScreen(selectedModel: RandomModel(title: "ONE"))
+            })
             
             Button("Button 2") {
 //                selectedIndex = 2
-                selectedModel = RandomModel(title: "TWO")
-                showSheet.toggle()
+//                selectedModel = RandomModel(title: "TWO")
+                showSheet2.toggle()
             }
+            .sheet(isPresented: $showSheet2, content: {
+                NextScreen(selectedModel: RandomModel(title: "TWO"))
+            })
         }
-        .sheet(isPresented: $showSheet, content: {
-            NextScreen(selectedModel: $selectedModel)
-//            if selectedIndex == 1 {
-//                NextScreen(selectedModel: RandomModel(title: "ONE"))
-//            } else if selectedIndex == 2 {
-//                NextScreen(selectedModel: RandomModel(title: "TWO"))
-//            } else {
-//                NextScreen(selectedModel: RandomModel(title: "STARTING TITLE"))
-//            }
-        })
+//        .sheet(isPresented: $showSheet, content: {
+//            NextScreen(selectedModel: selectedModel)
+////            if selectedIndex == 1 {
+////                NextScreen(selectedModel: RandomModel(title: "ONE"))
+////            } else if selectedIndex == 2 {
+////                NextScreen(selectedModel: RandomModel(title: "TWO"))
+////            } else {
+////                NextScreen(selectedModel: RandomModel(title: "STARTING TITLE"))
+////            }
+//        })
     }
 }
 
 struct NextScreen: View {
     
-    @Binding var selectedModel: RandomModel
+    let selectedModel: RandomModel
     
     var body: some View {
         Text(selectedModel.title)
