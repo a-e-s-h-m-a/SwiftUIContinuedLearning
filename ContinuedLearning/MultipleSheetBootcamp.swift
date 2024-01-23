4148de8608cf5da132b3b2e7fec6a196a125adbb
Aeshma <madhushanka2727@gmail.com>
2024-01-23 22:21:41 +0530

    the problem again

--- ContinuedLearning/MultipleSheetBootcamp.swift
+++ ContinuedLearning/MultipleSheetBootcamp.swift
@@ -16,21 +16,31 @@ struct MultipleSheetBootcamp: View {
     
     @State var selectedModel: RandomModel = RandomModel(title: "STARTING TITLE")
     @State var showSheet: Bool = false
+    @State var selectedIndex: Int = 0
     
     var body: some View {
         VStack(spacing: 20) {
             Button("Button 1") {
+                selectedIndex = 1
                 selectedModel = RandomModel(title: "ONE")
                 showSheet.toggle()
             }
             
             Button("Button 2") {
+                selectedIndex = 2
                 selectedModel = RandomModel(title: "TWO")
                 showSheet.toggle()
             }
         }
         .sheet(isPresented: $showSheet, content: {
-            NextScreen(selectedModel: selectedModel)
+//            NextScreen(selectedModel: selectedModel)
+            if selectedIndex == 1 {
+                NextScreen(selectedModel: RandomModel(title: "ONE"))
+            } else if selectedIndex == 2 {
+                NextScreen(selectedModel: RandomModel(title: "TWO"))
+            } else {
+                NextScreen(selectedModel: RandomModel(title: "STARTING TITLE"))
+            }
         })
     }
 }
