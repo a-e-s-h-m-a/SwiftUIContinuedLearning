4a5a19e92ab0a49570933ef3f41479061e297b9d
Aeshma <madhushanka2727@gmail.com>
2024-02-11 20:33:49 +0530

    Countdown to date

--- ContinuedLearning/TimerBootcamp.swift
+++ ContinuedLearning/TimerBootcamp.swift
@@ -21,8 +21,22 @@ struct TimerBootcamp: View {
      */
     
     // Countdown
+    /*
     @State var count: Int = 10
     @State var finishedText: String? = nil
+     */
+    
+    // Countdown to date
+    @State var timeRemaining: String = ""
+    let futureDate: Date = Calendar.current.date(byAdding: .day, value: 1, to: Date()) ?? Date()
+    
+    func updateTimeRemaining() {
+        let remaining = Calendar.current.dateComponents([.hour, .minute, .second], from: Date(), to: futureDate)
+        let hour = remaining.hour ?? 0
+        let minute = remaining.minute ?? 0
+        let second = remaining.second ?? 0
+        timeRemaining = "\(hour) : \(minute) : \(second)"
+    }
     
     var body: some View {
         ZStack {
@@ -34,18 +48,14 @@ struct TimerBootcamp: View {
             )
             .ignoresSafeArea()
             
-            Text(finishedText ?? "\(count)")
+            Text(timeRemaining)
                 .font(.system(size: 100, weight: .semibold, design: .rounded))
                 .foregroundStyle(.white)
                 .lineLimit(1)
                 .minimumScaleFactor(0.1)
         }
         .onReceive(timer, perform: { _ in
-            if count <= 1 {
-                finishedText = "Wow!"
-            } else {
-                count -= 1
-            }
+            updateTimeRemaining()
         })
     }
 }
