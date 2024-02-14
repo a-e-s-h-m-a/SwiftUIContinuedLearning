d0eaa7f6128b35a7af35bf3198b068e53e74d2bd
Aeshma <madhushanka2727@gmail.com>
2024-02-14 21:27:51 +0530

    saving to files

--- ContinuedLearning/FileManagerBootcamp.swift
+++ ContinuedLearning/FileManagerBootcamp.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
+class LocalFileManager {
+    static let instance = LocalFileManager()
+    
+    func saveImage(image: UIImage, name: String) {
+        guard let data = image.jpegData(compressionQuality: 1.0) else {
+            print("Error occured")
+            return
+        }
+        
+//        let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        //        let directory3 = FileManager.default.temporaryDirectory
+        
+        let directory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
+        let path = directory?.appendingPathComponent("\(name).jpg")
+        
+        guard
+            let path = FileManager
+                .default
+                .urls(for: .cachesDirectory, in: .userDomainMask)
+                .first?
+                .appendingPathComponent("\(name).jpg")
+        else {
+            print("Error getting path")
+            return
+        }
+
+        do {
+            try data.write(to: path)
+            print("Success saving...")
+        } catch let error {
+            print("Error saving \(error)")
+        }
+    }
+}
+
 class FileManagerViewModel: ObservableObject {
     
     @Published var image: UIImage? = nil
     let imageName = "dog"
+    let manager = LocalFileManager.instance
     
     init() {
         getImageFromAssetsFolder()
@@ -18,6 +54,11 @@ class FileManagerViewModel: ObservableObject {
     
     func getImageFromAssetsFolder() {
         image = UIImage(named: imageName)
+    }
+    
+    func saveImage() {
+        guard let image = image else { return }
+        manager.saveImage(image: image, name: imageName)
     }
     
 }
@@ -39,7 +80,7 @@ struct FileManagerBootcamp: View {
                 }
                 
                 Button(action: {
-                    
+                    vm.saveImage()
                 }, label: {
                     Text("Save to file manager")
                         .font(.headline)
