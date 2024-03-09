4de11ab978b1432caad78126c6205c79b7970949
Aeshma <madhushanka2727@gmail.com>
2024-03-09 21:50:27 +0530

    ex 2

--- ContinuedLearning/VisualEffectBootcamp.swift
+++ ContinuedLearning/VisualEffectBootcamp.swift
@@ -12,25 +12,40 @@ struct VisualEffectBootcamp: View {
     @State private var showSpacer: Bool = false
     
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-                .padding()
-    //            .frame(width: 400)
-                .background(.red)
-                .visualEffect { content, geometryProxy in
-                    content
-                        .grayscale(geometryProxy.frame(in: .global).minY < 300 ? 1 : 0)
-                        //.grayscale(geometryProxy.size.width >= 200 ? 1 : 0)
+        
+        ScrollView {
+            VStack(spacing: 30) {
+                ForEach(0..<100) { index in
+                    Rectangle()
+                        .frame(width: 300, height: 200)
+                        .frame(maxWidth: .infinity)
+                        .background(.orange)
+                        .visualEffect { content, geometryProxy in
+                            content
+                                .offset(x: geometryProxy.frame(in: .global).minY * 0.5)
+                        }
                 }
-            
-            if showSpacer {
-                Spacer()
             }
         }
-        .animation(.easeIn, value: showSpacer) // add animation outside of the visual effect modifier
-        .onTapGesture {
-            showSpacer.toggle()
-        }
+//        VStack {
+//            Text("Hello, World!")
+//                .padding()
+//    //            .frame(width: 400)
+//                .background(.red)
+//                .visualEffect { content, geometryProxy in
+//                    content
+//                        .grayscale(geometryProxy.frame(in: .global).minY < 300 ? 1 : 0)
+//                        //.grayscale(geometryProxy.size.width >= 200 ? 1 : 0)
+//                }
+//            
+//            if showSpacer {
+//                Spacer()
+//            }
+//        }
+//        .animation(.easeIn, value: showSpacer) // add animation outside of the visual effect modifier
+//        .onTapGesture {
+//            showSpacer.toggle()
+//        }
             
     }
 }
